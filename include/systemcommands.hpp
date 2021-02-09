5c26f6d2b7c8136fe22f9070f6d8f62fb52b6e04
Fii Firmware Team <69603166+Fii-USA@users.noreply.github.com>
2021-02-09 12:21:26 -0600

    fii-ipmi-oem : support for system & bios commands
    Update README.md
    fii oem command skeleton
    
    Signed-off-by: Lancelot Kao <lancelot.kao@fii-usa.com>
    Signed-off-by: Mohaimen alsmarai <Mohaimen.alsamarai@fii-na.com>

--- include/systemcommands.hpp
+++ include/systemcommands.hpp
@@ -1,52 +1,22 @@
-/*
-// Copyright (c) 2020 Foxconn Corporation
-//
-//
-*/
+/********************************************************************************
+*                       HON HAI Precision IND.Co., LTD.                         *
+*            Personal Computer & Enterprise Product Business Group              *
+*                      Enterprise Product Business Group                        *
+*                                                                               *
+*     Copyright (c) 2010 by FOXCONN/CESBG/CABG/SRD. All rights reserved.        *
+*     All data and information contained in this document is confidential       *
+*     and proprietary information of FOXCONN/CESBG/CABG/SRD and all rights      *
+*     are reserved. By accepting this material the recipient agrees that        *
+*     the information contained therein is held in confidence and in trust      *
+*     and will not be used, copied, reproduced in whole or in part, nor its     *
+*     contents revealed in any manner to others without the express written     *
+*     permission of FOXCONN/CESBG/CABG/SRD.                                     *
+*                                                                               *
+********************************************************************************/
 
 #pragma once
-#include <ipmid/api.hpp>
-#include <ipmid/utils.hpp>
-#include <nlohmann/json.hpp>
 
-enum fxn_sys_cmds
+enum fii_sys_cmds
 {
-    CMD_SYS_HW_CONFIG = 0x00,
-    CMD_SYS_GET_SYS_FW_VER = 0x01,
+    FII_CMD_SYS_PCIE_INFO = 0x03,
 };
-
-enum fxn_firm_cases
-{
-    BMC = 0x01,
-    BIOS = 0x02,
-    CPLD = 0x03,
-    MOTHERBOARD = 0x04,
-    PSU = 0x05,
-    FRU_VPD = 0x06,
-    OPENBMC = 0x07,
-};
-
-enum fxn_cpld_index
-{
-    MB = 0x01,
-    MP = 0x02,
-};
-
-enum fxn_hardware_config
-{
-    NCSI = 0x01,
-    VGA = 0x02,
-};
-
-enum fxn_hardware_mode
-{
-    CURRENT_CONFIG = 0x00,
-    ONBOARD_INTER = 0x01,
-    REAR_VGA = 0x01,
-    OPC3_0 = 0x02,
-    FRONT_VGA = 0x02,
-};
-
-const char *versionPath="/etc/foxconn/version.json";
-static constexpr const char* OpenBMCVersionPurpose = "xyz.openbmc_project.Software.Version.VersionPurpose.BMC";
-static constexpr const char* BiosVersionPurpose = "xyz.openbmc_project.Software.Version.VersionPurpose.HOST";
