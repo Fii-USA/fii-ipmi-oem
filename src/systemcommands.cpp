41ede4e606643bd426389e0340e56aa2af236932
Mustafa Shehabi <mustafa.shehabi@fii-na.com>
2021-09-01 11:31:48 -0500

    fii-usa/fii-ipmi-oem:  Modify PCIe bifurcation command internals
    Modify PCIe bifurcation and PCIe count to use fii-cpld driver instead
    of reading using i2cget directly
    
    Signed-off-by: Mustafa Shehabi <mustafa.shehabi@fii-na.com>

--- src/systemcommands.cpp
+++ src/systemcommands.cpp
@@ -16,73 +16,70 @@
 
 #include <common.hpp>
 #include <systemcommands.hpp>
-
-#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
 
 namespace ipmi
-{
-static void registerSystemFunctions() __attribute__((constructor));
+    {
+    static void registerSystemFunctions() __attribute__((constructor));
 
-ipmi::RspType<std::vector<uint8_t>> FiiSysPCIeInfo(boost::asio::yield_context yield)
-{
-	std::vector<uint8_t> rsp;
-	char buffer[128], *token;
-	uint32_t value;
-	FILE *pipe = popen(PCIEINFO_COMMAND, "r");
+    ipmi::RspType<std::vector<uint8_t>> FiiSysPCIeInfo(boost::asio::yield_context yield)
+    {
+        std::vector<uint8_t> rsp;
+        char *token;
+        uint32_t value;
 
-	// Read pcie bifurcation information
-	// it return two bytes, 1st byte bifurcation, 2nd byte present pin
-	if (!pipe) throw std::runtime_error("popen() failed !!!");
-	while (fgets(buffer, sizeof(buffer), pipe) != NULL)
-	{
-		std::cerr << " Command : " << buffer << std::endl;
-	}
-	pclose(pipe);
+        std::ifstream infile;
+        try {
+            infile.open(PCIEINFO_REG);
+        }
+        catch (const std::ifstream::failure& e) {
+            std::cerr << "Error opening/reading file" << std::endl;
+        }
+        std::stringstream strStream;
 
-	token = std::strtok(buffer, " ");
-	if (token == NULL)
-	{
-		phosphor::logging::log<phosphor::logging::level::ERR>(
-			"Fii system cmd : Error geting PCIe Info came back null");
-		ipmi::responseUnspecifiedError();
-	}
-	token = std::strtok(NULL, " ");
-	while (token != NULL)
-	{
-		//std::cerr << " Command token: " << token << std::endl;
-		value = std::stoul(token, nullptr, 16);
-		//std::cerr << " Command value: " << value << ":" << std::hex << value << std::endl;
-		rsp.push_back(static_cast<uint8_t>(value & 0xFF));
-		token = std::strtok(NULL, " ");
-	}
+        strStream << infile.rdbuf();
+        char *buffer = (char *) strStream.str().c_str();
 
-	return ipmi::responseSuccess(rsp);
+        infile.close();
+
+        while (1)   //Remove trailing white spaces
+        {
+            int len = std::strlen(buffer);
+            if(buffer[len - 1] == ' ' || buffer[len - 1] == '\n')
+                buffer[len - 1] = '\0';
+            else
+                break;
+        }
+
+        token = std::strtok(buffer, " ");
+        if (token == NULL)
+        {
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                "Fii system cmd : Error geting PCIe Info came back null");
+            ipmi::responseUnspecifiedError();
+        }
+        while (token != NULL)
+        {
+            //std::cerr << " Command token: " << token << std::endl;
+            value = std::stoul(token, nullptr, 16);
+            //std::cerr << " Command value: " << value << ":" << std::hex << value << std::endl;
+            rsp.push_back(static_cast<uint8_t>(value & 0xFF));
+            token = std::strtok(NULL, " ");
+        }
+
+        return ipmi::responseSuccess(rsp);
+    }
+
+    void registerSystemFunctions()
+    {
+        std::fprintf(stderr, "Registering OEM:[0x34], Cmd:[%#04X] for Fii System OEM Commands\n", FII_CMD_SYS_PCIE_INFO);
+        ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnOemThree, FII_CMD_SYS_PCIE_INFO, ipmi::Privilege::User,
+                FiiSysPCIeInfo);
+
+        return;
+    }
 }
 
-auto ipmiAppGetSystemIfCapabilities(uint8_t iface)
-    -> ipmi::RspType<uint8_t, uint8_t, uint8_t, uint8_t>
-{
 
-    // Per IPMI 2.0 spec, the input and output buffer size must be the max
-    // buffer size minus one byte to allocate space for the length byte.
-    constexpr uint8_t reserved = 0x00;
-    constexpr uint8_t support = 0b10000000;
-    constexpr uint8_t inMsgSize = 240;
-    constexpr uint8_t outMsgSize = 240;
-
-    return ipmi::responseSuccess(reserved, support,
-                                 inMsgSize, outMsgSize);
-}
-
-void registerSystemFunctions()
-{
-	std::fprintf(stderr, "Registering OEM:[0x34], Cmd:[%#04X] for Fii System OEM Commands\n", FII_CMD_SYS_PCIE_INFO);
-	ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnOemThree, FII_CMD_SYS_PCIE_INFO, ipmi::Privilege::User,
-					FiiSysPCIeInfo);
-	std::fprintf(stderr, "Registering APP:[0x06], Cmd:[%#04X] for Fii System OEM Commands\n", ipmi::app::cmdGetSystemIfCapabilities);
-    ipmi::registerHandler(ipmi::prioOemBase, ipmi::netFnApp, ipmi::app::cmdGetSystemIfCapabilities, ipmi::Privilege::User,
-                          ipmiAppGetSystemIfCapabilities);
-	return;
-}
-
-}
